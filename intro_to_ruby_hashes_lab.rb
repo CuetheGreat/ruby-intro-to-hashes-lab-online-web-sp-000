67ec67446a7caf4a4b6f6b175e2e7342032252ea
Clayon Stewart <c.stewart89@gmail.com>
2021-03-09 06:38:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,8 +34,8 @@ def monopoly_with_third_tier
 	    name: {},
 	    rent_in_dollars: {
 	      one_piece_owned: 25,
-	      two_peiceOwned: 50
-	      two_peiceOwned: 50
+	      two_peice_powned: 50
+	      three_peice_Owned: 50
 	      two_peiceOwned: 50
 	    }
 	  }
